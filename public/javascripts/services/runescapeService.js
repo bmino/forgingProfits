05c89c99bef7c12f52f2cb20c8d27d31f29f4cc4
bmino <brandonmino@bellsouth.net>
2016-07-23 22:38:57 -0400

    Proof of Concept

---
+++ public/javascripts/services/runescapeService.js
@@ -0,0 +1,27 @@
+angular.module('services')
+.service('runescapeService', function($http) {
+	
+	this.getPrice = function(itemId) {
+		var request = $http({
+			method: "get",
+			url: "api/rs/price/"+itemId
+		});
+		return request.then( handleSuccess, handleError );
+	};
+	
+	
+	
+	function handleSuccess(response) {
+		var successMessage = response.data.message;
+		return successMessage;
+	}
+	
+	function handleError(response) {
+		var dummy = document.createElement('body');
+		dummy.innerHTML = response.data;
+		var errorMessage = dummy.getElementsByTagName("h1")[0].innerHTML;
+		throw errorMessage;
+		return null;
+	}
+	
+});
---
+++ routes/runescape.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var runescape = require('runescape-api');
+
+/* ************** */
+/* Grand Exchange */
+/* ************** */
+
+/* GET categories information */
+router.get('/category/:category', function(req, res, next) {
+	var _category = req.params.category;
+	runescape.rs.ge.category(_category).then(function(data) {
+		res.json({message: data});
+	});
+});
+
+/* GET current price */
+router.get('/price/:itemId', function(req, res, next) {
+	var _id = req.params.itemId;;
+	runescape.rs.ge.graphData(_id).then(function(data) {
+		
+		var obj = data.daily;
+		var prices = Object.keys(obj).map(function (key) {return obj[key]});
+		var price = prices[prices.length-1];
+		
+		res.json({message: price});
+	});
+});
+
+/* GET graph data */
+router.get('/graphData/:itemId', function(req, res, next) {
+	var _id = req.params.itemId;;
+	runescape.rs.ge.graphData(_id).then(function(item) {
+		res.json({message: item});
+	});
+});
+
+/* GET item information */
+router.get('/itemInformation/:itemId', function(req, res, next) {
+	var _id = req.params.itemId;;
+	runescape.rs.ge.itemInformation(_id).then(function(item) {
+		res.json({message: item});
+	});
+});
+
+module.exports = router;
